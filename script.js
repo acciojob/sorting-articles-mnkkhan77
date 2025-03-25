35f6ab1c92161f89517eff8a2a6111bc38b02d53
Nasir Khan <37503821+mnkkhan77@users.noreply.github.com>
2025-03-25 19:52:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const bands = [
+      "The Plot in You",
+      "The Devil Wears Prada",
+      "Pierce the Veil",
+      "Norma Jean",
+      "The Bled",
+      "Say Anything",
+      "The Midway State",
+      "We Came as Romans",
+      "Counterparts",
+      "Oh, Sleeper",
+      "A Skylit Drive",
+      "Anywhere But Here",
+      "An Old Dog"
+    ];
+
+    function strip(bandName) {
+      return bandName.replace(/^(a |an |the )/i, "").trim();
+    }
+
+    const sortedBands = bands.sort((a, b) => strip(a).localeCompare(strip(b)));
+
+    const bandsList = document.querySelector('#bands');
+    if (bandsList) {
+      bandsList.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+    }
+  });
